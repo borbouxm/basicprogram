df2685eac93ff05760cb950c4e61eb7c9bc6b543
borbouxm <maxime.borboux@outlook.be>
2024-12-14 13:37:34 +0100

    Add files via upload

--- Exercice24/main.c
+++ Exercice24/main.c
@@ -6,24 +6,45 @@ int main()
     int heure, minute, seconde;
     printf("Entrez l'heure\n");
     scanf("%d:%d:%d", &heure, &minute, &seconde);
-    if(++seconde/60==1)
-    {
-        if(++minute/60==1)
+    if(heure<=23 && heure > 0)
         {
-            if(++heure/24==1)
+            if(minute<=59 && minute > 0)
             {
-                printf("00:00:00");
-            }
+                if(seconde<=59 && seconde > 0)
+                {
+                    if(++seconde/60==1)
+                    {
+                        if(++minute/60==1)
+                        {
+                            if(++heure/24==1)
+                            {
+                                printf("Une seconde plus tard, il sera 00:00:00");
+                            }
+                            else
+                            {
+                                printf("Une seconde plus tard, il sera %d:%d:%d", heure, 00, 00);
+                            }
+                        }
+                        else printf("Une seconde plus tard, il sera %d:%d:%d",heure, minute, 00);
+                    }
+                    else
+                    {
+                        printf("Une seconde plus tard, il sera %d:%d:%d", heure, minute, seconde);
+                    }
+                }
             else
+                {
+                printf("Seconde pas valide!");
+                }
+            }
+        else
             {
-                printf("%d:%d:%d", heure, 00, 00);
+                printf("Minute pas valide !");
             }
         }
-        else printf("%d:%d:%d",heure, minute, 00);
-    }
     else
     {
-        printf("%d:%d:%d", heure, minute, seconde);
+        printf("heure non valide!\n");
     }
     return 0;
 }

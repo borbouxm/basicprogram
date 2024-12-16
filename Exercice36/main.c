1f1f06bd947840a416894259f192642dd8272936
borbouxm <maxime.borboux@outlook.be>
2024-12-16 11:11:46 +0100

    Add files via upload

---
+++ Exercice36/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int pos=0,i=0,numberMax=0, number;
+    do
+    {
+        i++;
+        printf("\nEntrez le nombre numéro %d : ",i);
+        scanf("%d", &number);
+        if(number>numberMax)
+        {
+            pos = i;
+            numberMax=number;
+        }
+    }while(number!=0);
+    printf("Le plus grand de ces nombres est : %d\n", numberMax);
+    printf("C'était le nombre numéro %d\n", pos);
+    return 0;
+}
